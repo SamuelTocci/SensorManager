8473ba7abc05a8efa484eb1522b4c81b93a738d6
Samuel Tocci <samuel.tocci@student.kuleuven.be>
2021-10-11 17:35:50 +0200

    finished ex3

---
+++ ex3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define FREQ 2
+#define TEMP_MIN 5
+#define TEMP_MAX 25
+
+void main(void){
+	srand(time(0));
+
+	while(1){
+		printf("Temperature = %d @", TEMP_MIN + rand()%(TEMP_MAX - TEMP_MIN +1));
+		fflush(stdout);
+		system("date");
+		sleep(FREQ);
+	}
+
+
+
+}
