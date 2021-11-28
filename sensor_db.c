663a74b1f8a86d527b4e773bd22727a03f54e8b3
Samuel Tocci <samuel.tocci@student.kuleuven.be>
2021-11-28 13:49:04 +0100

    cleanup

--- sensor_db.c
+++ sensor_db.c
@@ -15,12 +15,6 @@
 
 typedef int (*callback_t)(void *, int, char **, char **);
 
-/**
- * Make a connection to the database server
- * Create (open) a database with name DB_NAME having 1 table named TABLE_NAME  
- * \param clear_up_flag if the table existed, clear up the existing data when clear_up_flag is set to 1
- * \return the connection for success, NULL if an error occurs
- */
 DBCONN *init_connection(char clear_up_flag){
 	sqlite3 *conn;
 	char *err_msg = 0;
@@ -42,22 +36,10 @@ DBCONN *init_connection(char clear_up_flag){
 	return conn;
 }
 
-/**
- * Disconnect from the database server
- * \param conn pointer to the current connection
- */
 void disconnect(DBCONN *conn){
 	sqlite3_close(conn);
 }
 
-/**
- * Write an INSERT query to insert a single sensor measurement
- * \param conn pointer to the current connection
- * \param id the sensor id
- * \param value the measurement value
- * \param ts the measurement timestamp
- * \return zero for success, and non-zero if an error occurs
- */
 int insert_sensor(DBCONN *conn, sensor_id_t id, sensor_value_t value, sensor_ts_t ts){
 	char *query;
 	char *err_msg = 0;
@@ -73,21 +55,8 @@ int insert_sensor(DBCONN *conn, sensor_id_t id, sensor_value_t value, sensor_ts_
 	return 0;
 }
 
-/**
- * Write an INSERT query to insert all sensor measurements available in the file 'sensor_data'
- * \param conn pointer to the current connection
- * \param sensor_data a file pointer to binary file containing sensor data
- * \return zero for success, and non-zero if an error occurs
- */
-int insert_sensor_from_file(DBCONN *conn, FILE *sensor_data);
+int insert_sensor_from_file(DBCONN *conn, FILE *sensor_data);//TODO
 
-/**
-  * Write a SELECT query to select all sensor measurements in the table 
-  * The callback function is applied to every row in the result
-  * \param conn pointer to the current connection
-  * \param f function pointer to the callback method that will handle the result set
-  * \return zero for success, and non-zero if an error occurs
-  */
 int find_sensor_all(DBCONN *conn, callback_t f){
 	char *query;
 	char *err_msg = 0;
@@ -102,14 +71,6 @@ int find_sensor_all(DBCONN *conn, callback_t f){
 	return 0;
 }
 
-/**
- * Write a SELECT query to return all sensor measurements having a temperature of 'value'
- * The callback function is applied to every row in the result
- * \param conn pointer to the current connection
- * \param value the value to be queried
- * \param f function pointer to the callback method that will handle the result set
- * \return zero for success, and non-zero if an error occurs
- */
 int find_sensor_by_value(DBCONN *conn, sensor_value_t value, callback_t f){
 	char *query;
 	char *err_msg = 0;
@@ -124,14 +85,6 @@ int find_sensor_by_value(DBCONN *conn, sensor_value_t value, callback_t f){
 	return 0;
 }
 
-/**
- * Write a SELECT query to return all sensor measurements of which the temperature exceeds 'value'
- * The callback function is applied to every row in the result
- * \param conn pointer to the current connection
- * \param value the value to be queried
- * \param f function pointer to the callback method that will handle the result set
- * \return zero for success, and non-zero if an error occurs
- */
 int find_sensor_exceed_value(DBCONN *conn, sensor_value_t value, callback_t f){
 	char *query;
 	char *err_msg = 0;
@@ -146,14 +99,6 @@ int find_sensor_exceed_value(DBCONN *conn, sensor_value_t value, callback_t f){
 	return 0;
 }
 
-/**
- * Write a SELECT query to return all sensor measurements having a timestamp 'ts'
- * The callback function is applied to every row in the result
- * \param conn pointer to the current connection
- * \param ts the timestamp to be queried
- * \param f function pointer to the callback method that will handle the result set
- * \return zero for success, and non-zero if an error occurs
- */
 int find_sensor_by_timestamp(DBCONN *conn, sensor_ts_t ts, callback_t f){
 	char *query;
 	char *err_msg = 0;
@@ -168,14 +113,6 @@ int find_sensor_by_timestamp(DBCONN *conn, sensor_ts_t ts, callback_t f){
 	return 0;
 }
 
-/**
- * Write a SELECT query to return all sensor measurements recorded after timestamp 'ts'
- * The callback function is applied to every row in the result
- * \param conn pointer to the current connection
- * \param ts the timestamp to be queried
- * \param f function pointer to the callback method that will handle the result set
- * \return zero for success, and non-zero if an error occurs
- */
 int find_sensor_after_timestamp(DBCONN *conn, sensor_ts_t ts, callback_t f){
 	char *query;
 	char *err_msg = 0;
